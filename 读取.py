3c98cfa2663d10c6d028af27e79fa350c029d133
100210110 <104227884+100210110@users.noreply.github.com>
2023-06-21 02:05:42 +0800

    快速显示已存储数据
    以更易读的方式显示json存储的数据

--- 读取.py
+++ 读取.py
@@ -20,19 +20,8 @@ print()
 employees_dict = data['employees']
 zones_dict = data['zones']
 
-for employee_id, values in employees_dict.items():
-    i += 1
-    print(f"第 {i} 名")
-    print("员工姓名:", employee_id)
-    for key, value in values.items():
-        print(key, ":", value)
-    print("-" * 20)  # 在员工之间添加分界线
-    
-print()             #看起来还……勉强易于分辨的分割线
-print("-" * 30)
-print()
-print("-" * 20)
 
+#显示区域属性
 for zone_id, zone_data in zones.items():
     print("区域名称:", zone_id)
     
@@ -42,5 +31,18 @@ for zone_id, zone_data in zones.items():
             print(f"\t{key}: {value}")
             
     print("-" * 20)  # 在每个区域之间添加分界线
-    
+
+print()             #看起来还……勉强易于分辨的分割线
+print("-" * 30)
+print()
+print("-" * 20)
+
+#显示员工属性
+for employee_id, values in employees_dict.items():
+    i += 1
+    print(f"第 {i} 名")
+    print("员工姓名:", employee_id)
+    for key, value in values.items():
+        print(key, ":", value)
+    print("-" * 20)  # 在员工之间添加分界线
 input()
