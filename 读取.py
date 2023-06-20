336f1c7ab4040a35ee51e81058a865c710e6db41
100210110 <104227884+100210110@users.noreply.github.com>
2023-06-21 01:59:44 +0800

    读取json
    可以读取已存储的json，以一个比较友好的视图呈现出来

--- 读取.py
+++ 读取.py
@@ -22,15 +22,17 @@ zones_dict = data['zones']
 
 for employee_id, values in employees_dict.items():
     i += 1
-    print(f"第{i}名")
+    print(f"第 {i} 名")
     print("员工姓名:", employee_id)
     for key, value in values.items():
         print(key, ":", value)
     print("-" * 20)  # 在员工之间添加分界线
-print()
+    
+print()             #看起来还……勉强易于分辨的分割线
 print("-" * 30)
 print()
 print("-" * 20)
+
 for zone_id, zone_data in zones.items():
     print("区域名称:", zone_id)
     
@@ -40,4 +42,5 @@ for zone_id, zone_data in zones.items():
             print(f"\t{key}: {value}")
             
     print("-" * 20)  # 在每个区域之间添加分界线
+    
 input()
